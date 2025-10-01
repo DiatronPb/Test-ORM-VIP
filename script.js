a42709353757685aaab58f18cdec095bade52412
DiatronPb <noah.louet-24@outlook.com>
2025-10-02 00:43:55 +0200

    essaie implémentation avec llm, à tester après datasimple

--- script.js
+++ script.js
@@ -3,16 +3,8 @@ const LAT_POLYTECH = 47.364604;
 const LNG_POLYTECH = 0.684748;
 
 
-const token_hf = null;
+const reqllm = true;
 
-/*
-try {
-    token_hf = ProcessingInstruction.env.HF_TOKEN;
-} catch (error) {
-    console.log("Erreur dans le chargement du token du llm")
-
-}
-*/
 
 
 // Initialisation de la carte centrée sur Paris
@@ -34,7 +26,11 @@ let currentCircle = null;
 
 let markerli = [];
 
-map.on('click', function(e) {
+let currentLoopId = 0; // éviter 2 boucles concurentes
+
+map.on('click', async function(e) {
+
+    const startTime = Date.now();
 
     //console.log(e);
     console.log("------click !!-----")
@@ -48,6 +44,9 @@ map.on('click', function(e) {
         currentCircle = null;
     }
     */
+    currentLoopId++;
+    const thisloopid = currentLoopId;
+
     markerli.forEach(el => {map.removeLayer(el)});
     markerli = [];
 
@@ -97,17 +96,60 @@ map.on('click', function(e) {
     `
     */
 
-    
-    const query4 = `
-    [out:json];
-    (
-    node(around:${radiusCirclem},${lat},${lng});
-    );
-    out body;
-    `
+    const zommte = map.getZoom();
+    let queryt = "";
+    if (zommte > 13){
+        queryt = `
+        [out:json];
+        (
+        node(around:${radiusCirclem},${lat},${lng});
+        );
+        out body;
+        `;
+    }
+    else if (zommte > 10){
+        queryt = `
+        [out:json];
+        (
+        node["place"="city"](around:${radiusCirclem},${lat},${lng}); 
+        node["place"="town"](around:${radiusCirclem},${lat},${lng}); 
+        node["place"="village"](around:${radiusCirclem},${lat},${lng}); 
+        );
+        out body;
+        `;
+
+    }
+    else if (zommte > 7){
+        queryt = `
+        [out:json];
+        node["place"="city"](around:${radiusCirclem},${lat},${lng}); 
+        out body;
+        `;
+    }
+    else{
+        queryt = `
+        [out:json];
+        relation["admin_level"="2"]["boundary"="administrative"];
+        out body;
+        `;
+    }
+
+/*
+[out:json];
+area["name"="Indre-et-Loire"]["admin_level"="6"]->.searchArea;
+(
+  node["place"="village"](area.searchArea);
+  way["place"="village"](area.searchArea);
+  relation["place"="village"](area.searchArea);
+);
+out center;
+
+*/
+
+
 
     
-    const url = 'https://overpass-api.de/api/interpreter?data=' + encodeURIComponent(query4);
+    const url = 'https://overpass-api.de/api/interpreter?data=' + encodeURIComponent(queryt);
 
     console.log(url)
 
@@ -121,10 +163,10 @@ map.on('click', function(e) {
                 throw new Error(`Erreur HTTP : ${response.status}`)
             }
         })
-        .then(data => {
-            //console.log(data);
+        .then(async data => {
+            console.log(data);
             let datafiltered = data.elements.filter(el => {return el.tags && Object.keys(el.tags).length > maxZoom - map.getZoom()});
-            console.log(datafiltered);
+            //console.log(datafiltered);
             console.log(`J'ai trouvé : ${data.elements.length} élément(s)`);
 
             console.log(`J'ai trouvé : ${Object.keys(datafiltered).length} élément(s) après filtre`);
@@ -153,25 +195,42 @@ map.on('click', function(e) {
 
 
             })
-            
-            if (token_hf){
-                // trier ce qu'on envoie
+            console.log(datafiltered);
+            if (reqllm){
+                try{
+                    console.log("appel llm");
+                    const datasimple = datafiltered.map(el => {return Object.entries(el.tags).map(([k, v]) => {`${k} : ${v}`})});
 
+                    const rep = await generateDesc(datasimple);
+                    const cleaned = rep.replace(/```json|```/g, '').trim();
+                    const parsed = JSON.parse(cleaned);
+                    console.log(parsed);
+                }catch(err){
+                    console.log("Erreur LLM :", err);
+                }
+                
+                // trier ce qu'on envoie
+                /*
                 generateDesc(datafiltered).then(rep => {
                     const cleaned = rep.replace(/```json|```/g,'').trim();
                     const parsed = JSON.parse(cleaned)
                     console.log(parsed);
-
-                    
                 });
+                */
+
+
             }
 
-            console.log(markerli);
+            //console.log(markerli);
+            const endtime = Date.now();
+            console.log(`Temps écoulé : ${((endtime - startTime) / 1000).toFixed(2)} s`);
 
             // à adapter avec le json récupérer quand j'aurai trouvé une solution pour le llm
-            (async () => {
+            if (zommte > 7){
                 let cpt = 0;
                 while (cpt < datafiltered.length){
+                    //console.log(cpt);
+                    if (currentLoopId != thisloopid) break;
                     const nodedesc = Object.entries(datafiltered[cpt].tags).map(([key, value]) => `${key} : ${value}`).join("<br>");
                     //console.log(nodedesc);
                     let marker = markerli[cpt+1];
@@ -187,8 +246,9 @@ map.on('click', function(e) {
                         fillColor : "green"
                     });
                     cpt ++;
+
                 }
-            })();
+            }
 
             
 
@@ -204,13 +264,8 @@ map.on('click', function(e) {
 
 map.on('zoomstart', () => {
     markerli.forEach(el => {map.removeLayer(el)});
- 
-    /*
-    if (currentCircle) {
-        map.removeLayer(currentCircle);
-        currentCircle = null;
-    }
-    */
+    currentLoopId++;
+    console.log(map.getZoom())
 });
 
 
@@ -228,6 +283,41 @@ async function generateDesc(dat){
         ...
     }`
     ;
+    try {
+        const response = await fetch("http://127.0.0.1:3000/chat", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "mistral-7b-instruct-v0.1.Q4_0.gguf",
+            messages: [
+                { 
+                    role: "system",
+                    content: "Tu es un assistant qui transforme des tags OSM en phrases très courtes pour un lecteur d'écran." 
+                },
+                { 
+                    role: "user", 
+                    content: prompt 
+                }
+                ]
+            })
+        });
+
+        console.log("Requête envoyée, réponse reçue");
+
+        const data = await response.json();
+        console.log(data);
+        return data.choices[0].message.content; 
+        } 
+    catch (err) {
+            console.log("Erreur dans generatedesc :", err);
+            return "erreur llm";
+    }
+}
+
+
+/*
     const response = await fetch("https://router.huggingface.co/v1/chat/completions", {
         method: "POST",
         headers: {
@@ -243,14 +333,7 @@ async function generateDesc(dat){
             ]
         })
     });
-
-  const data = await response.json();
-  //console.log(data);
-  return data.choices[0].message.content;
-
-}
-
-
+*/
 
 
 
